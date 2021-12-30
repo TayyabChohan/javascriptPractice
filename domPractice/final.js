250ea3e6d74cc82007dd75d3b88731d6dae13204
TayyabChohan <TayyabChohan7@gmail.com>
2021-12-30 09:56:06 +0500

    final_commit

---
+++ domPractice/final.js
@@ -0,0 +1,83 @@
+/// select class in section
+// const selectSection = document.querySelector(".todo-dection");
+// console.log(selectSection.classList);
+
+///// add class
+// selectSection.classList.add('bg-background')
+/// remove container class
+// selectSection.classList.remove('container')
+// /// check a class if exist
+//  const dffd=selectSection.classList.contains('container')
+//  console.log(dffd)
+//  selectSection.classList.toggle('bg-background')
+
+////////////////////////////////////project started
+// const todolist = document.querySelector(".todo-list");
+// todolist.innerHTML = todolist.innerHTML + "<li> new todo </li>";
+//// or
+// todolist.innerHTML +="<li> new todo </li>";
+
+///////////////////////createElement
+// const newTodoitem=document.createElement('li')
+// const addTextTodoItem=document.createTextNode('new item')
+// newTodoitem.append(addTextTodoItem)
+// item.append(newTodoitem)
+// item.prepend(newTodoitem)
+// console.log(newTodoitem)
+
+// const item=document.querySelector('.todo-list li')
+// item.remove()
+//////////////
+
+// const newTodoitem=document.createElement('li')
+// addTextTodoItem.t
+///////////////////////////////////////////////////////////////////setInterval
+// const body = document.querySelector("body");
+// setInterval(() => {
+//   const red = Math.floor(Math.random() * 126);
+//   const green = Math.floor(Math.random() * 126);
+//   const blue = Math.floor(Math.random() * 126);
+//   const rgp=`rgb(${red},${green},${blue})`
+// //   console.log(rgp)
+//   body.style.background=rgp
+// },1000);
+// console.log(body);
+
+///////////////////callbackhell
+////////////////////callback hell ka hal promises se hai
+// setTimeout(() => {
+//   console.log("1");
+//   setTimeout(() => {
+//     console.log("2");
+//     setTimeout(() => {
+//       console.log("3");
+//       setTimeout(() => {
+//         console.log("4");
+//       }, 5000);
+//     }, 4000);
+//   }, 2000);
+// }, 1000);
+///////////////////////promise ,, future vulue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
